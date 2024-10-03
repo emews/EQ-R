77e462283b1c01c00ab129646fa4ff15d85070af
Justin Wozniak <woz@anl.gov>
2024-10-03 16:34:23 -0500

    Rename to EQR_CONDA

--- conda/osx-arm64/build.sh
+++ conda/osx-arm64/build.sh
@@ -5,13 +5,13 @@
 
 echo "build.sh: START"
 
-DEV_CONDA=$( cd $RECIPE_DIR/.. ; /bin/pwd -P )
+EQR_CONDA=$( cd $RECIPE_DIR/.. ; /bin/pwd -P )
 
 (
   export LDFLAGS="-ltcl8.6"
 
   set -x
-  $DEV_CONDA/build-generic.sh
+  $EQR_CONDA/build-generic.sh
 )
 
 echo "build.sh: STOP"
